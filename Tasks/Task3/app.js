35d962055a09e5b3a71925c9a08238ca3cd77ee9
Timur Bukin <bukintimur4@gmail.com>
2025-09-11 19:45:23 +0300

    Task3.2

--- Tasks/Task3/app.js
+++ Tasks/Task3/app.js
@@ -6,14 +6,27 @@ const listElement = document.getElementById('list')
 createBtn.onclick = function() {
     if(inputElement.value.length === 0) {return}
     const note = document.createElement('li')
+    note.classList.add('note')
+
     const text = document.createElement('span')
-    text.textContent = inputElement.value 
+    text.textContent = inputElement.value
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.className = 'delete-btn'
+    deleteBtn.textContent = 'Erase'
+
+    deleteBtn.onclick = function() {
+        note.remove();
+    };
+
     note.appendChild(text)
+    note.appendChild(deleteBtn)
     listElement.appendChild(note)
     inputElement.value = ''
+    inputElement.focus()
 }
 
-listElement.addEventListener('click', function(event) {
+/*listElement.addEventListener('click', function(event) {
     if (event.target.getAttribute('data-type') === 'remove') {
         const listItem = event.target.closest('li')
         if (listItem) {
@@ -21,3 +34,16 @@ listElement.addEventListener('click', function(event) {
         }
     }
 })
+
+function getNoteTemplate() {
+    return `
+    <li class="list-group-item d-flex 
+        justify-content-between
+        align-items-center">
+        <span>${inputElement}</span>
+        <span>
+            <span class="btn btn-small btn-danger" 
+            data-type="remove">&times;</span>
+        </span>
+    </li>`
+}*/
