c04a62d53a40e80415f00ada35f41d02d0168a65
Timur Bukin <bukintimur4@gmail.com>
2025-09-23 10:27:21 +0300

    Task6.1-Modal

--- Tasks/Task6/app.js
+++ Tasks/Task6/app.js
@@ -2,16 +2,16 @@ const modal = document.getElementById("myModal")
 const btn = document.getElementById("myBtn")
 const span = document.getElementsByClassName("close")[0]
 
-btn.onclick = function() {
+btn.addEventListener('click', function() {
     modal.style.display = "block"
-}
+})
 
-span.onclick = function() {
+span.addEventListener('click', function() {
     modal.style.display = "none"
-}
+})
 
-window.onclick = function(event) {
+window.addEventListener('click', function(event) {
     if(event.target == modal) {
         modal.style.display = "none"
     }
-}
+})
